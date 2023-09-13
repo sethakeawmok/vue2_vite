c7453e240591143cf1050a8a34747aa7ef8d4a66
sethakeawmok <setha40@live.com>
2023-09-13 17:04:22 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+const { createVuePlugin } = require("vite-plugin-vue2");
+const { resolve } = require("path");
+
+module.exports = {
+  plugins: [createVuePlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "nested/index.html")
+      }
+    }
+  },
+  server: {
+    port: 1234,
+  },
+};
